e61323edc6769502dd0d17ff69b8bbb5b378c892
William B. Swift <willbswift@hotmail.com>
2019-01-10 16:20:12 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Books from "./pages/Books";
+import Nav from "./components/Nav";
+
+        import logo from "./logo.svg";
+        import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Books />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
